8456986f958bdbacc1256bcb7611a612aa60e8ff
hugo escandon <hescandon6@gmail.com>
2020-07-31 14:23:20 -0400

    info added

---
+++ src/components/info.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Info = () => {
+  return(
+
+<div>
+Phone (908) 380-4770 Email hescandon6@gmail.com LinkedIn hugo-escandon GitHub @Git-push-01
+</div>
+)
+}
+
+export default Info
--- src/containers/menuContainer.js
+++ src/containers/menuContainer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Tab } from 'semantic-ui-react'
 import Bio from "../components/bio"
+import Info from "../components/info"
 
 const panes = [
   { menuItem: 'Bio', render: () => <Tab.Pane><Bio/></Tab.Pane> },
-  { menuItem: 'Info', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+  { menuItem: ' Contact Info', render: () => <Tab.Pane><Info/></Tab.Pane> },
   { menuItem: 'Technical Skills', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
   { menuItem: 'Education', render: () => <Tab.Pane>Tab 3s Content</Tab.Pane> }
 ]
