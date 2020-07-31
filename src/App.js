f979b51d8be9852d8c567e8298333a2e8fd3b531
hugo escandon <hescandon6@gmail.com>
2020-07-31 08:54:23 -0400

    semantic-ui menu and portfolioContiner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import Menu from './menu'
+import PortfolioContainer from './portfolioContainer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <h1>Hugo Portfolio App</h1>
+    <Menu/>
+    
+    <PortfolioContainer/>
+
+
     </div>
   );
 }
