aa253642f4264309b6150041de7918b710a85a53
hugo escandon <hescandon6@gmail.com>
2020-08-07 10:12:24 -0400

    OnMouseHover Tabs

--- src/containers/menuContainer.js
+++ src/containers/menuContainer.js
@@ -1,22 +1,60 @@
-import React from 'react'
-import { Tab, Popup  } from 'semantic-ui-react'
-import Bio from "../components/bio"
-import Info from "../components/info"
-import Education from "../components/education"
-import TechSkill from "../components/techSkill"
+import React,{ useState } from "react";
+import { Tab, Popup } from "semantic-ui-react";
+import Bio from "../components/bio";
+import Info from "../components/info";
+import Education from "../components/education";
+import TechSkill from "../components/techSkill";
 
 const panes = [
-  { menuItem: 'Bio', render: () =><Tab.Pane><Bio/></Tab.Pane>},
-  { menuItem: ' Contact Info', render: () => <Tab.Pane><Info/></Tab.Pane> },
-  { menuItem: 'Technical Skills', render: () => <Tab.Pane><TechSkill/></Tab.Pane> },
-  { menuItem: 'Education', render: () => <Tab.Pane><Education/></Tab.Pane> }
-]
+  {
+    menuItem: "Bio",
+    render: () => (
+      <Tab.Pane>
+        <Bio />
+      </Tab.Pane>
+    ),
+  },
+  {
+    menuItem: " Contact Info",
+    render: () => (
+      <Tab.Pane>
+        <Info />
+      </Tab.Pane>
+    ),
+  },
+  {
+    menuItem: "Technical Skills",
+    render: () => (
+      <Tab.Pane>
+        <TechSkill />
+      </Tab.Pane>
+    ),
+  },
+  {
+    menuItem: "Education",
+    render: () => (
+      <Tab.Pane>
+        <Education />
+      </Tab.Pane>
+    ),
+  },
+];
 
-const MenuContainer = () => (
+const MenuContainer = () => {
 
- <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={panes}  />
+const [isShown, notShown] = useState(false);
 
 
-)
+    return (
 
+      <Tab
+       onMouseEnter={()=>isShown(true) }
+       onMouseLeave={()=>notShown(false)}
+       menu={{ fluid: true, vertical: true, tabular: true }}
+       panes={panes}
+      />
+
+    );
+
+}
 export default MenuContainer
