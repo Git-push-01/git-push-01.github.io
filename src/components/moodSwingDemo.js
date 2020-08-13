9fb3d3504a73889109e24db53b5733fe894980cd
hugo escandon <hescandon6@gmail.com>
2020-08-13 09:38:37 -0400

    update Portfolio Container with Tabs

--- src/components/moodSwingDemo.js
+++ src/components/moodSwingDemo.js
@@ -5,10 +5,10 @@ import moodSwing from "../images/moodSwing.jpeg";
 const MoodSwingDemo = () => (
   <Card fluid={true} raised={true}>
     <Card.Content>
-      <Card.Header>Break Time App Demo</Card.Header>
+      <Card.Header>Mood Swing Demo</Card.Header>
       <Card.Header>
-        Break-time scheduler for frontline employees to keep track of
-        break-times and improve efficiency
+      Music video app that utilizes combinations of emojis to deliver a preloaded video playlist, with user customization
+
       </Card.Header>
       <Card.Description>
         <Embed
--- src/containers/portfolioContainer.js
+++ src/containers/portfolioContainer.js
@@ -1,51 +1,18 @@
-import React, { Component } from "react";
-import { Menu, Segment} from "semantic-ui-react";
+import React from 'react'
+import { Tab } from 'semantic-ui-react'
 import BreakTimeDemo from "../components/breakTimeDemo"
 import MoodSwingDemo from "../components/moodSwingDemo"
 
-export default class PortfolioContainer extends Component{
-  state = { activeItem: "1" };
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+const panes = [
+  {
+    menuItem: 'Break Time Demo',
+    render: () => <Tab.Pane ><BreakTimeDemo/></Tab.Pane>,
+  },
+  { menuItem: 'Mood Swing Demo', render: () => <Tab.Pane><MoodSwingDemo/></Tab.Pane> },
+  { menuItem: 'Dog Barks Demo', render: () => <Tab.Pane>Dog Bark Demo</Tab.Pane> },
+]
 
-  render() {
-    const { activeItem } = this.state;
+const PortfolioContainer = () => <Tab panes={panes} />
 
-    return (
-      <div>
-        <Segment attached="top">Technical Projects</Segment>
-
-        <Menu attached="bottom" tabular icon>
-          <Menu.Item
-
-            name="1"
-            active={activeItem === "1"}
-            onClick={this.handleItemClick}
-          >
-            Mood Swing
-            <MoodSwingDemo/>
-          </Menu.Item>
-
-          <Menu.Item
-            name="2"
-            active={activeItem === "2"}
-            onClick={this.handleItemClick}
-          >
-            Break Time
-
-
-            <BreakTimeDemo/>
-          </Menu.Item>
-
-          <Menu.Item
-            name="3"
-            active={activeItem === "3"}
-            onClick={this.handleItemClick}
-          >
-          Dog Barks
-          </Menu.Item>
-        </Menu>
-      </div>
-    );
-  }
-}
+export default PortfolioContainer
