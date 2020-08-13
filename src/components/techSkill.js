e6730c5ce630b14876e958a1e3a26af5ab12b512
hugo escandon <hescandon6@gmail.com>
2020-08-13 10:34:29 -0400

    TechSkills Update

--- src/components/techSkill.js
+++ src/components/techSkill.js
@@ -8,11 +8,43 @@ const TechSkill = () => (
 
       <Card.Description>
         <img
-          src="https://img.icons8.com/color/144/000000/javascript.png"
+          src="https://img.icons8.com/color/96/000000/javascript.png"
           alt=""
         />
         <img
-          src="https://img.icons8.com/color/144/000000/postgreesql.png"
+          src="https://img.icons8.com/plasticine/96/000000/react.png"
+          alt=""
+        />
+        <img
+          src="https://img.icons8.com/color/96/000000/redux.png"
+          alt=""
+        />
+        <img
+          src="https://img.icons8.com/color/96/000000/ruby-programming-language.png"
+          alt=""
+        />
+        <img
+          src="https://img.icons8.com/color/96/000000/postgreesql.png"
+          alt=""
+        />
+        <img
+          src="https://img.icons8.com/color/96/000000/html-5.png"
+          alt=""
+        />
+        <img
+          src="https://img.icons8.com/color/96/000000/css3.png"
+          alt=""
+        />
+        <img
+          src="https://img.icons8.com/color/96/000000/npm.png"
+          alt=""
+        />
+        <img
+          src="https://img.icons8.com/color/96/000000/heroku.png"
+          alt=""
+        />
+        <img
+          src="https://img.icons8.com/windows/96/000000/nodejs.png"
           alt=""
         />
       </Card.Description>
