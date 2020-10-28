7981b6318dbc6bd700ca839eb3e14d0de28c8642
Hugo Escandon <hescandon6@gmail.com>
2020-10-28 15:27:14 -0400

    UPDATE BIO

--- src/components/bio.js
+++ src/components/bio.js
@@ -2,16 +2,12 @@ import React from "react";
 import { Segment, Image } from "semantic-ui-react";
 import hugoPic from "../images/hugoPic.jpg";
 
-
 const Bio = () => (
   <Segment
     inverted
     textAlign="center"
     as="h2"
-
-
     style={{
-
       fontSize: "1.5em",
       fontWeight: "normal",
       marginTop: "0.5em",
@@ -19,13 +15,14 @@ const Bio = () => (
     }}
   >
     <Image size="medium" src={hugoPic} centered />
-    Team-oriented Full Stack Software Developer with a passion for developing
-    creative business solutions. Using my background in Computer Science and
-    experience in Operations, I bring adaptability to challenging environments,
-    with a detail-oriented approach to problem solving. I have a collaborative
-    work ethic and a strong desire to learn new technologies. I am eager to
-    utilize my skills in Javascript, React, Redux, Ruby, and Rails to
-    effectively deliver the goals that my employer has set out to reach.
+    Team-oriented Full Stack Software Developer; with a passion for developing
+    creative business solutions. Using my background in Computer Science,
+    interpersonal, and communications skills, I bring adaptability to
+    challenging environments, with a detailed-oriented approach to
+    problem-solving. I have a strong desire to learn new technologies and a
+    collaborative work ethic. I am eager to apply my skillset in Javascript,
+    React, Redux, Ruby, and Rails to effectively deliver the goals that my
+    employer has set out to reach.
   </Segment>
 );
 
